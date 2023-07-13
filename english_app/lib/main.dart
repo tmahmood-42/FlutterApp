27192daa3eaba4190b49860436329a2f70f32b8b
tmahmood-42 <muser5743@gmail.com>
2023-07-12 18:43:29 -0700

    Firebase

--- english_app/lib/main.dart
+++ english_app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart'; // Firebase auth package
+import 'package:cloud_firestore/cloud_firestore.dart'; //firestroe package
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
